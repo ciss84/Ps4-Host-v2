228c25c0dc4a0ee82ccac81f11e8aed7449e893b
Mugiwara <37186888+ciss84@users.noreply.github.com>
2022-12-19 13:39:08 +0100

    Add files via upload

--- payload.js
+++ payload.js
@@ -47,7 +47,7 @@ function loadHomeBrew() {
     }else {
         document.getElementById('notify').style.display = ''
         document.getElementById('notify').className = 'notification'
-        setTimeout(function(){document.getElementById('log').innerHTML ='<h1 style=color:#ebfa89>*Jailbreak Done, Now load HEN Manually!!!</h1>'; }, 50);   
+        setTimeout(function(){document.getElementById('log').innerHTML ='<h1 style=color:#ebfa89>*Jailbreak Done, Now Unplug USB!!!</h1>'; }, 50);   
         setTimeout(ani2, 4000);
     }
 }
@@ -55,9 +55,8 @@ function loadHomeBrew() {
 function load_usb(){
     document.getElementById('notify').style.display = ''
     document.getElementById('notify').className = 'notification'
-    setTimeout(function(){document.getElementById('log').innerHTML ='<h1 style=color:#ebfa89>*Insert Usb Now !!!...</h1>'; }, 3000);
-    setTimeout(function(){document.getElementById("log").innerHTML="<h1 style=color:#ebfa89>Unplug Usb Now !!!...</h1>"; }, 3000);
-    setTimeout(ani2, 6000);
+    setTimeout(function(){document.getElementById('log').innerHTML ='<h1 style=color:#ebfa89>*Insert Usb Now !!!...</h1>'; }, 100);
+    setTimeout(ani2, 4000);
 }       
 
 function autopl(){
