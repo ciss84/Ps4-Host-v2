255b38b74afbbf9611c9f4c5ebc69667b7e0992d
Mugiwara <37186888+ciss84@users.noreply.github.com>
2022-12-19 15:05:59 +0100

    Add files via upload

--- payload.js
+++ payload.js
@@ -55,8 +55,7 @@ function loadHomeBrew() {
 function load_usb(){
     document.getElementById('notify').style.display = ''
     document.getElementById('notify').className = 'notification'
-    setTimeout(function(){document.getElementById('log').innerHTML ='<h1 style=color:#ebfa89>*Insert Usb Now !!!...</h1>'; }, 3000);
-    setTimeout(function(){document.getElementById("log").innerHTML="<h1 style=color:#ebfa89>Unplug Usb Now !!!...</h1>"; }, 3000);
+    setTimeout(function(){document.getElementById('log').innerHTML ='<h1 style=color:#ebfa89>*Insert Usb Now !!!...</h1>'; }, 50);
     setTimeout(ani2, 6000);
 }       
 
